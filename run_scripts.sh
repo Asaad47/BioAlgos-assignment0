c25d6786993a80f32c2edaa99a2624a29709b5c7
Asaad47 <75583625+Asaad47@users.noreply.github.com>
2025-02-13 10:34:08 +0300

    Rename prob2 file

--- run_scripts.sh
+++ run_scripts.sh
@@ -13,10 +13,10 @@ prob1_kmp() {
     /usr/bin/time -l go run prob1_kmp.go ncbi_dataset_GRCh38/ncbi_dataset/data/GCA_000001405.29/GCA_000001405.29_GRCh38.p14_genomic.fna
 }
 
-prob2_naive() {
-    echo "Running commands for prob2_naive"
-    /usr/bin/time -l go run prob2_naive.go ncbi_dataset_T2T/ncbi_dataset/data/GCA_009914755.4/GCA_009914755.4_T2T-CHM13v2.0_genomic.fna
-    /usr/bin/time -l go run prob2_naive.go ncbi_dataset_GRCh38/ncbi_dataset/data/GCA_000001405.29/GCA_000001405.29_GRCh38.p14_genomic.fna
+prob2() {
+    echo "Running commands for prob2"
+    /usr/bin/time -l go run prob2.go ncbi_dataset_T2T/ncbi_dataset/data/GCA_009914755.4/GCA_009914755.4_T2T-CHM13v2.0_genomic.fna
+    /usr/bin/time -l go run prob2.go ncbi_dataset_GRCh38/ncbi_dataset/data/GCA_000001405.29/GCA_000001405.29_GRCh38.p14_genomic.fna
 }
 
 
@@ -28,19 +28,19 @@ if [[ $# -eq 1 ]]; then
         prob1_kmp)
             prob1_kmp
             ;;
-        prob2_naive)
-            prob2_naive
+        prob2)
+            prob2
             ;;
         *)
             echo ">>> Running all commands."
             prob1_naive
             prob1_kmp
-            prob2_naive
+            prob2
             ;;
     esac
 else
     echo ">>> Running all scripts."
     prob1_naive
     prob1_kmp
-    prob2_naive
+    prob2
 fi
